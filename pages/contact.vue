57cc92b7eac3539550ba7ac9b88f3f423a1b0d94
Ibrahim Asif <ibrahimasif@192.168.1.5>
2026-03-02 19:29:31 +0500

    Fix Netlify form submission

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,30 +5,19 @@
     <form
       name="contact"
       method="POST"
+      action="/thank-you"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
-      action="/thank-you"
     >
-      <!-- required for Netlify -->
+      <!-- Required for Netlify -->
       <input type="hidden" name="form-name" value="contact" />
       <p style="display:none">
         <label>Don’t fill this out: <input name="bot-field" /></label>
       </p>
 
-      <label>
-        Name
-        <input name="name" type="text" required />
-      </label>
-
-      <label>
-        Email
-        <input name="email" type="email" required />
-      </label>
-
-      <label>
-        Message
-        <textarea name="message" rows="6" required></textarea>
-      </label>
+      <input type="text" name="name" placeholder="Your Name" required />
+      <input type="email" name="email" placeholder="Your Email" required />
+      <textarea name="message" placeholder="Your Message" required></textarea>
 
       <button type="submit">Send</button>
     </form>
